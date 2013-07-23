439536001d172fecf1721a71953dbd145296a516
Adam Candy <adam.candy@imperial.ac.uk>
2013-07-24 00:50:13 +0100

    Initial import of the QGIS-plugins-meshing code

---
+++ dev/plugins/define_boundary_ids/__init__.py
@@ -0,0 +1,57 @@
+"""
+
+##########################################################################
+#  
+#  QGIS-meshing plugins.
+#  
+#  Copyright (C) 2012-2013 Imperial College London and others.
+#  
+#  Please see the AUTHORS file in the main source directory for a
+#  full list of copyright holders.
+#  
+#  Dr Adam S. Candy, adam.candy@imperial.ac.uk
+#  Applied Modelling and Computation Group
+#  Department of Earth Science and Engineering
+#  Imperial College London
+#  
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation,
+#  version 2.1 of the License.
+#  
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#  
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+#  USA
+#  
+##########################################################################
+
+/***************************************************************************
+Name			 	 : Define Boundary Ids
+Description          : Assigns boundary ids for identification in fluidity.
+Date                 : 19/Jul/12 
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+def name(): 
+  return "Define Boundary Ids" 
+def description():
+  return "Assigns boundary ids for identification in fluidity."
+def version(): 
+  return "Version 1.1" 
+def qgisMinimumVersion():
+  return "1.0"
+def classFactory(iface): 
+  # load Define_Boundary_Ids class from file Define_Boundary_Ids
+  from Define_Boundary_Ids import Define_Boundary_Ids 
+  return Define_Boundary_Ids(iface)
